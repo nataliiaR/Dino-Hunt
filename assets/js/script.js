cef670e2fed04a1b2850a50c477d3b029fd14c4a
Nataliia Ryzha <ryzha89@gmail.com>
2019-06-13 23:30:44 -0700

    updated the layout and added images

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+  var hunter = "";
+  var dino = "";
+  var hunter_life;
+  var dino_life = "";
+  var isHunterSelected = false;
+  var isDinoSelected = false;
+
+
+  $('.hunter').on('click', function () {
+
+
+  });
+
+
+  $('.dino').on('click', function () {
+    
+  });
+
+  $('#attack').on('click', function () {
+
+  });
+});
