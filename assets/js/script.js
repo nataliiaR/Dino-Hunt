50b1b319bd2d43b81299d322c014e5bae0a63664
Nataliia Ryzha <ryzha89@gmail.com>
2019-06-15 10:43:54 -0700

    adding logic to attack functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ const triceratops = new Dino(200,30);
       isHunterSelected = true;
       $('.hunter-name').css("background-color","darkgreen");
       $('.hunter-life').css("background-color","#556B2F");
-      
+      hunterSelected =this;
    }
    else{
     $("#hunter-1").hide();
@@ -31,6 +31,7 @@ const triceratops = new Dino(200,30);
 
       $('.hunter-name').css("background-color","darkgreen");
       $('.hunter-life').css("background-color","#556B2F");
+      hunterSelected = this;
     }
    
 
@@ -64,6 +65,7 @@ const triceratops = new Dino(200,30);
           
 
         }
+        selectedDino=this;
         console.log($(".dino-name",this).text() + " dino "+ dino.life);
        
   }
@@ -75,9 +77,36 @@ const triceratops = new Dino(200,30);
       if(!isHunterSelected || !isDinoSelected){
      alert("no player or dino");
       }
-      /*else{
-        attackTheDino();
-      }*/
+      else if (distanceToHunter>0)
+        attackTheDino(dino);
+  
   });
 
+
+var distanceToHunter=200;
+
+  function attackTheDino(dino){
+    dinoStep = dino.speed+Math.floor(Math.random() * 30);
+    penetrate = 60+Math.floor(Math.random() * 10);
+    dino.life=dino.life-penetrate;
+    $(".dino-life",selectedDino).text(dino.life);
+
+    if (distanceToHunter>0){
+
+    distanceToHunter = distanceToHunter-dinoStep;
+    console.log("distance to hunter "+distanceToHunter);
+
+    }
+    if(dino.life<0){
+      alert("dino got killed");
+    }
+    if (distanceToHunter<0) {
+      console.log("do I get here");
+        //$(".hunter-life",hunterSelected).text(0);
+         alert("Hunter got eaten " + $(".hunter-life",hunterSelected).text());
+    }
+    
+   
+  }
+  
  
