78306abb032916e3089c438bda4d4f68dc819bcf
Nataliia Ryzha <ryzha89@gmail.com>
2019-06-14 22:44:42 -0700

    added hunter/dino selection and dino objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,79 @@
   var dino_life = "";
   var isHunterSelected = false;
   var isDinoSelected = false;
-
-
-  $('.hunter').on('click', function () {
-
-
-  });
-
-
-  $('.dino').on('click', function () {
-    if(!isHunterSelected){
-        alert("select the hunter first");
+  var dino;
+  class Dino {
+    constructor(life, speed) {
+        this.life = life;
+        this.speed = speed;
     }
+}
+const tyrannosaurus = new Dino(150,20);
+const rex = new Dino(300,50);
+const triceratops = new Dino(200,30);
+
+  $('div.hunter').on('click', function () {
+  console.log($(this).attr('id'));
+   if( $(this).attr('id')==="hunter-1"){
+     $("#hunter-2").hide();
+      isHunterSelected = true;
+      $('.hunter-name').css("background-color","darkgreen");
+      $('.hunter-life').css("background-color","#556B2F");
+      
+   }
+   else{
+    $("#hunter-1").hide();
+      isHunterSelected =true;
+
+      $('.hunter-name').css("background-color","darkgreen");
+      $('.hunter-life').css("background-color","#556B2F");
+    }
+   
+
   });
+
+
+  $('.dino-box').on('click', function () {
+    console.log(isHunterSelected);
+
+    if(isHunterSelected && !isDinoSelected) {
+    
+        $(".dino-name",this).css("background-color","darkgreen");
+        $(".dino-life",this).css("background-color","#556B2F");
+        isDinoSelected=true;
+        
+        switch($(".dino-name",this).text()){
+          
+          case "Tyrannosaurus":
+          dino=tyrannosaurus;
+          break;
+
+          case "Rex":
+          dino=rex;
+          break;
+
+
+          case "Triceratops":
+
+          dino=triceratops;
+          break;
+          
+
+        }
+        console.log($(".dino-name",this).text() + " dino "+ dino.life);
+       
+  }
+  });
+
+
 
   $('#attack').on('click', function () {
       if(!isHunterSelected || !isDinoSelected){
      alert("no player or dino");
       }
+      /*else{
+        attackTheDino();
+      }*/
   });
+
+ 
