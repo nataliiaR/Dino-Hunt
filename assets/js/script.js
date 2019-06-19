533d0f5da41252fc189eedc0eab98a2fb4e029c5
Nataliia Ryzha <ryzha89@gmail.com>
2019-06-18 22:58:50 -0700

    styles adjusting

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,12 +20,15 @@ const triceratops = new Dino(300,30);
 var killedDinos=0;
 var killedHunter=0;
 
+var distanceToHunter=200+Math.floor(Math.random() * 100);
+
+//audio
 var gunAudio = new Audio("assets/audio/Skorpion-Kibblesbob.mp3");
 var arrowAudio = new Audio("assets/audio/Throw_Knife.mp3")
 var rexAudio = new Audio("assets/audio/Tyrannosaurus Rex -SoundBible.mp3");
 var triceratopsAudio = new Audio("assets/audio/Roar-SoundBible.mp3");
 
-
+//reset dino selection
 function resetDinoSelection(){
   distanceToHunter=distanceToHunter+50+Math.floor(Math.random() * 100);
   isDinoSelected=false;
@@ -33,7 +36,7 @@ function resetDinoSelection(){
   $(".hunter-life",hunterSelected).css("color","white");
 
   }
-
+//select a hunter
   $('div.hunter').on('click', function () {
    if( $(this).attr('id')==="hunter-1"){
       $("#hunter-2").hide();
@@ -48,7 +51,6 @@ function resetDinoSelection(){
       var divelement = document.getElementById(attribute);
       $(".hunterZone").append(divelement);
       $(".battleArea").css("display","block");
-
    }
    else{
       $("#hunter-1").hide();
@@ -69,52 +71,39 @@ function resetDinoSelection(){
 
   });
 
-
+//select a dino
   $('.dino-box').on('click', function (e) {
     var killed= $(".dino-life",this).text();
     if(isHunterSelected && !isDinoSelected && killed!=="KILLED")  {
-    
-        $(".dino-name",this).css("background-color","darkgreen");
-        $(".dino-life",this).css("background-color","#556B2F");
-        isDinoSelected=true;
+      $(".dino-name",this).css("background-color","darkgreen");
+      $(".dino-life",this).css("background-color","#556B2F");
+      isDinoSelected=true;
         
-        switch($(".dino-name",this).text()){
-          
-          case "Tyrannosaurus":
+      switch($(".dino-name",this).text()){ 
+        case "Tyrannosaurus":
           dino=tyrannosaurus;
           dinoAudio=rexAudio;
           break;
-
-          case "Herrerasaurus":
+        case "Herrerasaurus":
           dino=herrerasaurus;
           dinoAudio=rexAudio;
           break;
-
-
-          case "Triceratops":
-
+        case "Triceratops":
           dino=triceratops;
           dinoAudio=triceratopsAudio;
           break;
-          
-
-        }
+      }
         selectedDino=this;
         $('#attack').css("display","block");
         $(".dino-life",this).text("Dino's life " +dino.life);
         $(".hunter-life",hunterSelected).text("Distance to dino "+ distanceToHunter);
         attribute= $(this).attr("id");
-
-        //$('').css('display',"none");
         var divelement = document.getElementById(attribute);
         $(".battleZone").prepend(divelement);
-
-     
-  }
+    }
   });
 
-
-
+//play a game 
   $('#attack').on('click', function () {
    if (distanceToHunter>0 && isDinoSelected===true) {
         huntAudio.load();
@@ -124,43 +113,43 @@ function resetDinoSelection(){
   
   });
 
- distanceToHunter=200+Math.floor(Math.random() * 100);
 
   function attackTheDino(dino){
     $(".hunter-life",hunterSelected).text("Distance to dino "+ distanceToHunter);
     dinoStep = dino.speed+Math.floor(Math.random() * 30);
     penetrate = 60+Math.floor(Math.random() * 70);
     dino.life=dino.life-penetrate;
-    $(".dino-life",selectedDino).text("Life remaining " +dino.life);
+
     dinoAudio.load();
-    setTimeout(function() {
+
+    //play dino roar audio
+    audioDino=setTimeout(function() {
       dinoAudio.play();
-    }, 1000);
+    }, 500);
 
+        
+    $(".dino-life",selectedDino).text("Life remaining " +dino.life);
     if (distanceToHunter>0){
-
     distanceToHunter = distanceToHunter-dinoStep;
-    
-
     }
+
     if (distanceToHunter<=50){
       $(".hunter-life",hunterSelected).css("color","orange");
     }
 
     if(dino.life<0){
-
       $(".dino-life",selectedDino).text("KILLED");
       $(".dino-name",selectedDino).css("background-color","red");
       $(".dino-life",selectedDino).css("background-color","red");
       $(".hunter-life",hunterSelected).text("");
       killedDinos=killedDinos+1;
       attribute= $(selectedDino).attr("id");
-      console.log(attribute);
       var divelement = document.getElementById(attribute);
       $(".dino-section").append(divelement);
-      console.log(divelement);
+      clearTimeout(audioDino);
       resetDinoSelection();
     }
+
     if (distanceToHunter<=0) {
       $(".hunter-life",hunterSelected).css("color","white");
       $(".hunter-life",hunterSelected).text("KILLED");     
@@ -171,28 +160,21 @@ function resetDinoSelection(){
 
     if(killedHunter===1){
       $(".gameIsOverLose").css("display","block");
-      $('#attack').text("PLAY");
+      $('#attack').text("PLAY AGAIN");
       $('#attack').attr("class","reset");
       $('.reset').on('click', function () {
-     
         location.reload();
-       
        });
     
     } 
     if(killedDinos===3){
-
       $(".gameIsOverWin").css("display","block");
-      $('#attack').text("PLAY");
+      $('#attack').text("PLAY AGAIN");
       $('#attack').attr("reset","reset");
       $('.reset').on('click', function () {
         location.reload();
-       
        });
-    
     }
-    
-   
   }
 
 
